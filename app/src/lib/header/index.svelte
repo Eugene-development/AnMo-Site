97f15e9de49a8943e5eb2520ffd24f268278673f
indexpro24@gmail.com <indexpro24@gmail.com>
2022-03-08 17:20:48 +0300

    git init

--- app/src/lib/header/index.svelte
+++ app/src/lib/header/index.svelte
@@ -18,8 +18,8 @@
 
 <header>
     <div class="relative">
-        <div class="bg-gray-900 p-4">
-            <nav class="relative max-w-full mx-1 flex items-center justify-between px-1 sm:px-2" aria-label="Global">
+        <div class="bg-black p-4">
+            <nav class="relative max-w-full mx-8 flex items-center justify-between px-1 sm:px-2" aria-label="Global">
                 <div class="flex items-center flex-1">
                     <div class="flex items-center justify-between w-full md:w-auto">
                         <a href="/">
@@ -60,8 +60,11 @@
                     </nav>
 
                 </div>
-                <div class="hidden lg:flex md:items-center md:space-x-6">
-                    <p class="text-base font-medium text-white hover:text-gray-300 lg:text-xl">
+                <div class="hidden lg:flex md:items-center md:space-x-4 text-white">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 3l-6 6m0 0V4m0 5h5M5 3a2 2 0 00-2 2v1c0 8.284 6.716 15 15 15h1a2 2 0 002-2v-3.28a1 1 0 00-.684-.948l-4.493-1.498a1 1 0 00-1.21.502l-1.13 2.257a11.042 11.042 0 01-5.516-5.517l2.257-1.128a1 1 0 00.502-1.21L9.228 3.683A1 1 0 008.279 3H5z" />
+                    </svg>
+                    <p class="text-xl font-medium  hover:text-gray-300 lg:text-2xl">
                         {phone}
                     </p>
                 </div>
--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -2,8 +2,30 @@
 	<title>Эротический массаж в Москве</title>
 </svelte:head>
 
+<div class="">
+	<div class="relative max-w-fool">
+		<div class="relative py-48 px-8 overflow-hidden lg:px-16 lg:grid lg:grid-cols-2 lg:gap-x-8">
+			<div class="absolute inset-0  mix-blend-multiply">
+				<img src="https://img3.badfon.ru/original/1920x1200/c/74/devushka-blondinka-v-krasnom.jpg" alt="" class="w-full h-full object-cover">
+			</div>
+			<div class="relative lg:col-span-1 text-gray-50">
+				<h1 class="text-7xl font-bold ">Эротический массаж</h1>
+<!--				<img class="h-12 w-auto" src="https://tailwindui.com/img/logos/workcation-logo-white.svg" alt="">-->
+				<blockquote class="mt-6 ">
+					<p class="text-xl font-medium sm:text-2xl">Ты устал от ежедневных забот и ищешь способ отвлечься и отдохнуть? Приходи к нам за блаженным релаксом для души и тела!</p>
+					<footer class="mt-6">
+						<p class="flex flex-col font-medium">
+							<span>Администратор Ангелина &#128105;</span>
+						</p>
+					</footer>
+				</blockquote>
+			</div>
+		</div>
+	</div>
+</div>
 
-<div class="bg-white">
+
+<div class="bg-black">
 	<div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl">
 		<div class="relative rounded-lg overflow-hidden lg:h-96">
 			<div class="absolute inset-0">
@@ -13,10 +35,10 @@
 			<div aria-hidden="true" class="relative w-full h-32 lg:hidden"></div>
 			<div class="absolute inset-x-0 bottom-0 bg-black bg-opacity-75 p-6 rounded-bl-lg rounded-br-lg backdrop-filter backdrop-blur sm:flex sm:items-center sm:justify-between lg:inset-y-0 lg:inset-x-auto lg:w-96 lg:rounded-tl-lg lg:rounded-br-none lg:flex-col lg:items-start">
 				<div>
-					<h2 class="text-xl font-bold text-white">Workspace Collection</h2>
-					<p class="mt-1 text-sm text-gray-300">Upgrade your desk with objects that keep you organized and clear-minded.</p>
+					<h2 class="text-xl font-bold text-white">Наши девушки</h2>
+					<p class="mt-1 text-sm text-gray-300">Каждая мастерица владеет уникальным навыком, который вы узнаете только на сеансе </p>
 				</div>
-				<a href="#" class="mt-6 flex-shrink-0 flex bg-white bg-opacity-0 py-3 px-4 border border-white border-opacity-25 rounded-md items-center justify-center text-base font-medium text-white hover:bg-opacity-10 sm:mt-0 sm:ml-8 lg:ml-0 lg:w-full">View the collection</a>
+				<a href="/girls" class="mt-6 flex-shrink-0 flex bg-white bg-opacity-0 py-3 px-4 border border-white border-opacity-25 rounded-md items-center justify-center text-base font-medium text-white hover:bg-opacity-10 sm:mt-0 sm:ml-8 lg:ml-0 lg:w-full">Посмотреть всех</a>
 			</div>
 		</div>
 	</div>
