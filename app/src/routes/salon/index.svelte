36570911debd30d26dd96e05902a94dc471358eb
indexpro24@gmail.com <indexpro24@gmail.com>
2022-03-08 18:55:44 +0300

    git init

---
+++ app/src/routes/salon/index.svelte
@@ -0,0 +1,46 @@
+<div class="bg-black">
+    <div aria-hidden="true" class="relative">
+        <img src="https://tailwindui.com/img/ecommerce-images/product-feature-02-full-width.jpg" alt="" class="w-full h-96 object-center object-cover">
+        <div class="absolute inset-0 bg-gradient-to-t from-black"></div>
+    </div>
+
+    <div class="relative -mt-12 max-w-7xl mx-auto pb-16 px-4 sm:pb-24 sm:px-6 lg:px-8">
+        <div class="max-w-2xl mx-auto text-center lg:max-w-4xl">
+            <h2 class="text-3xl font-extrabold tracking-tight text-gray-50 sm:text-4xl">Наш салон</h2>
+            <p class="mt-4 text-gray-50">Organize is a system to keep your desk tidy and photo-worthy all day long. Procrastinate your work while you meticulously arrange items into dedicated trays.</p>
+        </div>
+
+        <dl class="mt-16 max-w-2xl mx-auto grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:max-w-none lg:grid-cols-3 lg:gap-x-8">
+            <div class="border-t border-red-700 pt-4">
+                <dt class="font-medium text-gray-200">Описание</dt>
+                <dd class="mt-2 text-sm text-gray-50">Designed by Good Goods, Inc.</dd>
+            </div>
+
+            <div class="border-t border-red-700 pt-4">
+                <dt class="font-medium text-gray-200">Описание</dt>
+                <dd class="mt-2 text-sm text-gray-50">Designed by Good Goods, Inc.</dd>
+            </div>
+
+            <div class="border-t border-red-700 pt-4">
+                <dt class="font-medium text-gray-200">Описание</dt>
+                <dd class="mt-2 text-sm text-gray-50">Designed by Good Goods, Inc.</dd>
+            </div>
+
+            <div class="border-t border-red-700 pt-4">
+                <dt class="font-medium text-gray-200">Описание</dt>
+                <dd class="mt-2 text-sm text-gray-50">Designed by Good Goods, Inc.</dd>
+            </div>
+
+            <div class="border-t border-red-700 pt-4">
+                <dt class="font-medium text-gray-200">Описание</dt>
+                <dd class="mt-2 text-sm text-gray-50">Designed by Good Goods, Inc.</dd>
+            </div>
+
+            <div class="border-t border-red-700 pt-4">
+                <dt class="font-medium text-gray-200">Описание</dt>
+                <dd class="mt-2 text-sm text-gray-50">Designed by Good Goods, Inc.</dd>
+            </div>
+
+        </dl>
+    </div>
+</div>
