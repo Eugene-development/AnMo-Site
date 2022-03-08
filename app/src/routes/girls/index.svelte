36de26ec21fb0af7f1dc3e4a66921c57a550d74d
indexpro24@gmail.com <indexpro24@gmail.com>
2022-03-08 18:20:53 +0300

    git init

--- app/src/routes/girls/index.svelte
+++ app/src/routes/girls/index.svelte
@@ -4,7 +4,7 @@
         <div class="space-y-12">
             <div class="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
                 <h2 class="text-3xl font-extrabold text-white tracking-tight sm:text-4xl">Наши мастерицы</h2>
-                <p class="text-xl text-gray-300">Ornare sagittis, suspendisse in hendrerit quis. Sed dui aliquet lectus sit pretium egestas vel mattis neque.</p>
+                <p class="text-xl text-gray-300">Выберите себе мастера по душе. Работает ли в данный момент она вы можете узнать по телефону.</p>
             </div>
             <ul role="list" class="space-y-4 sm:grid sm:grid-cols-2 sm:gap-6 sm:space-y-0 lg:grid-cols-3 lg:gap-8">
                 <li class="py-10 px-6 bg-gray-800 text-center rounded-lg xl:px-10 xl:text-left">
@@ -44,41 +44,3 @@
     </div>
 </div>
 
-
-<!-- This example requires Tailwind CSS v2.0+ -->
-<div class="rounded-lg bg-white overflow-hidden shadow">
-    <h2 class="sr-only" id="profile-overview-title">Profile Overview</h2>
-    <div class="bg-white p-6">
-        <div class="sm:flex sm:items-center sm:justify-between">
-            <div class="sm:flex sm:space-x-5">
-                <div class="flex-shrink-0">
-                    <img class="mx-auto h-20 w-20 rounded-full" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-                </div>
-                <div class="mt-4 text-center sm:mt-0 sm:pt-1 sm:text-left">
-                    <p class="text-sm font-medium text-gray-600">Welcome back,</p>
-                    <p class="text-xl font-bold text-gray-900 sm:text-2xl">Rebecca Nicholas</p>
-                    <p class="text-sm font-medium text-gray-600">Product Designer</p>
-                </div>
-            </div>
-            <div class="mt-5 flex justify-center sm:mt-0">
-                <a href="#" class="flex justify-center items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"> View profile </a>
-            </div>
-        </div>
-    </div>
-    <div class="border-t border-gray-200 bg-gray-50 grid grid-cols-1 divide-y divide-gray-200 sm:grid-cols-3 sm:divide-y-0 sm:divide-x">
-        <div class="px-6 py-5 text-sm font-medium text-center">
-            <span class="text-gray-900">12</span>
-            <span class="text-gray-600">Vacation days left</span>
-        </div>
-
-        <div class="px-6 py-5 text-sm font-medium text-center">
-            <span class="text-gray-900">4</span>
-            <span class="text-gray-600">Sick days left</span>
-        </div>
-
-        <div class="px-6 py-5 text-sm font-medium text-center">
-            <span class="text-gray-900">2</span>
-            <span class="text-gray-600">Personal days left</span>
-        </div>
-    </div>
-</div>
